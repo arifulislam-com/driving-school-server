d0cc66127dbfb328c93559bc5b9ec62987844329
arifulislam-com <arifulislam9565@gmail.com>
2021-06-27 03:49:09 +0600

    some changes

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 const MongoClient = require('mongodb').MongoClient;
 const { ObjectID, ObjectId } = require('mongodb');
 const fileUpload = require('express-fileupload');
+const fs = require('fs-extra')
 
 const app = express()
 app.use(bodyParser.json())
@@ -24,25 +25,73 @@ client.connect(err => {
   const coursesCollection = client.db("drivingSchool").collection("courses");
   const bookingCollection = client.db("drivingSchool").collection("bookingCourses");
   const reviewCollection = client.db("drivingSchool").collection("review");
+  const adminCollection = client.db("drivingSchool").collection("admin");
 
   app.post('/addCourse', (req, res) => {
     const file = req.files.file;
     const name = req.body.name;
     const price = req.body.price;
     console.log(name, price, file);
-    const path = `${__dirname}/courseImg/${file.name}`;
-    file.mv(path, err => {
+    const filePath = `${__dirname}/courseImg/${file.name}`;
+    file.mv(filePath, err => {
       if (err) {
         console.log(err);
-        return res.status(500).send({ msg: "failed to upload img" })
+        res.status(500).send({ msg: "failed to upload img" })
       }
-      return res.send({ name: file.name, path: `/${file.name}` })
+      const newImg = fs.readFileSync(filePath);
+      const encimg = newImg.toString('base64');
+      const image = {
+        contentType: req.files.file.mimetype,
+        size: req.files.file.size,
+        img: Buffer(encimg, 'base64')
+      };
+      coursesCollection.insertOne({name,price,image})
+      .then(result => {
+        fs.remove(filePath, error => {
+          if(error) {
+            res.status(500).send({ msg: "failed to upload img" })
+            console.log(error)
+          }
+          res.send(result.insertedCount > 0);
+        })
+        console.log(result);
+      })
+    })
+  });
+
+  app.get('/courses', (req, res) => {
+    coursesCollection.find({})
+    .toArray((err, document) => {
+      res.send(document)
+    })
+  });
+
+  app.get('/courseById/:id', (req, res) => {
+    const id = req.params.id;
+    coursesCollection.find({_id:ObjectId(id)})
+    .toArray((err, document) => {
+      res.send(document)
+    })
+  });
+
+  app.post('/makeAdmin', (req, res) => {
+    const email = req.body
+    adminCollection.insertOne(email)
+    .then(result => {
+      res.send(result.insertedCount > 0);
+    })
+  });
+  
+  app.post('/isAdmin', (req, res) => {
+    const email = req.body.email
+    adminCollection.find({email})
+    .toArray((err, document) => {
+      res.send(document.length > 0);
     })
   })
 
   app.post('/review', (req, res) => {
     const review = req.body;
-    console.log(review);
     reviewCollection.insertOne(review)
     .then(result => {
       res.send(result)
@@ -63,8 +112,7 @@ client.connect(err => {
     console.log(bookingdData);
     bookingCollection.insertOne(bookingdData)
     .then(result => {
-      console.log(result);
-      //res.send(result)
+      res.send(result.insertedCount > 0)
     })
   })
 
@@ -88,7 +136,6 @@ client.connect(err => {
         console.log(ObjectId(req.params.id));
         bookingCollection.deleteOne({_id:ObjectId(req.params.id)})
         .then(result => {
-          console.log(result);
             res.send("succes", result)
         })
     })
